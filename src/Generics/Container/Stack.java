e076eede3b82994ed2b0bf0c366b40e6da82ebf6
Martin Kribs <54333035+martinkribs@users.noreply.github.com>
2023-04-02 22:58:38 +0200

    added stack

--- src/Generics/Container/Stack.java
+++ src/Generics/Container/Stack.java
@@ -1,4 +1,58 @@
 package Generics.Container;
 
-public class Stack {
+import java.util.ArrayList;
+
+public class Stack<T> extends ArrayList<T> {
+
+    /**
+     * ArrayList
+     */
+    ArrayList<T> stack;
+
+    /**
+     * Stack
+     */
+    public Stack() {
+        this.stack = new ArrayList<>();
+    }
+
+    /**
+     * liefert true, wenn der Stack leer ist, ansonsten false.
+     *
+     * @return bool
+     */
+    public boolean empty() {
+        return this.stack == null;
+    }
+
+    /**
+     * legt item oben auf den Stack und gibt item zurück.
+     *
+     * @param item
+     * @return T
+     */
+    public T push(T item) {
+        this.stack.add(item);
+        return item;
+    }
+
+    /**
+     * löscht das oberste Element des Stacks und liefert es zurück.
+     *
+     * @return T
+     */
+    public T pop() {
+        T item = this.peek();
+        this.stack.remove(item);
+        return item;
+    }
+
+    /**
+     * liefert das oberste Element des Stacks.
+     *
+     * @return T
+     */
+    public T peek() {
+        return this.stack.get(this.stack.size() - 1);
+    }
 }
