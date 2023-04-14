17b467374a9f8b28f105dfa14a6a66f5236c0967
Martin Kribs <martin.kribs.mk@gmail.com>
2023-04-14 10:49:47 +0200

    sort

--- src/Generics/Container/Sortieren.java
+++ src/Generics/Container/Sortieren.java
@@ -18,14 +18,14 @@ public class Sortieren {
         Comparator<String> comp = new Comparator<>() {
             @Override
             public int compare(String o1, String o2) {
-                if (o1.length()<o2.length()){
+                if (o1.length() < o2.length()) {
                     return -1;
-                } else if(o1.length()== o2.length()){
+                } else if (o1.length() == o2.length()) {
                     return 0;
                 } else {
                     return 1;
                 }
-            } 
+            }
         };
 
         personList.sort(comp);
