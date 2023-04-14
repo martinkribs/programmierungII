33f3957da4d2cf64a4c8b4cb69f0db5d16d3e62a
Martin Kribs <martin.kribs.mk@gmail.com>
2023-04-14 10:50:34 +0200

    last error

--- src/Exception_Handling/Uebungen/TestPersonenalter.java
+++ src/Exception_Handling/Uebungen/TestPersonenalter.java
@@ -3,5 +3,6 @@ package Exception_Handling.Uebungen;
 public class TestPersonenalter {
     public static void main(String[] args) throws OutOfRangeException {
         Personenalter alter = new Personenalter(5);
+        System.out.println(alter);
     }
 }
