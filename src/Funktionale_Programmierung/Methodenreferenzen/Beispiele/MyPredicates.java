8197e5dbe66cd4d2cd7e1a04eb6d49e1ad0f27fd
martinkribs <martin.kribs.mk@gmail.com>
2023-04-21 11:12:31 +0200

    method references

---
+++ src/Funktionale_Programmierung/Methodenreferenzen/Beispiele/MyPredicates.java
@@ -0,0 +1,11 @@
+package Funktionale_Programmierung.Methodenreferenzen.Beispiele;
+
+public class MyPredicates {
+	public static boolean isEven(Integer n) {
+		return n % 2 == 0;
+	}
+
+	public static boolean isOdd(Integer n) {
+		return n % 2 != 0;
+	}
+}
---
+++ src/Funktionale_Programmierung/Methodenreferenzen/Uebungen/Sortieren.java
@@ -0,0 +1,5 @@
+package Funktionale_Programmierung.Methodenreferenzen.Uebungen;
+
+public class Sortieren {
+    
+}
