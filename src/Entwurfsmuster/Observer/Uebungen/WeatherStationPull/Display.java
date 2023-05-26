175c5a9d05643f05f19bc21c466c9c681c116e4d
martinkribs <martin.kribs.mk@gmail.com>
2023-05-26 10:45:37 +0200

    added remove

--- src/Entwurfsmuster/Observer/Uebungen/WeatherStationPull/Display.java
+++ src/Entwurfsmuster/Observer/Uebungen/WeatherStationPull/Display.java
@@ -1,12 +1,12 @@
 package Entwurfsmuster.Observer.Uebungen.WeatherStationPull;
 
-public class Display implements Observer{
+public class Display implements Observer {
 
     String name;
 
     WeatherStation weatherStation;
 
-    public Display(WeatherStation weatherStation ,String name){
+    public Display(WeatherStation weatherStation, String name) {
         this.name = name;
         this.weatherStation = weatherStation;
         this.weatherStation.registerDisplay(this);
@@ -15,11 +15,15 @@ public class Display implements Observer{
     @Override
     public void measurementChanged() {
         System.out.println("_______________________________________");
-        System.out.println("Wetterstation: " +this.name);
-        System.out.println("Temperatur: "+ this.weatherStation.getTemperature());
-        System.out.println("Feuchtigkeit: "+ this.weatherStation.getHumidity());
-        System.out.println("Druck: "+ this.weatherStation.getPressure());
+        System.out.println("Wetterstation: " + this.name);
+        System.out.println("Temperatur: " + this.weatherStation.getTemperature());
+        System.out.println("Feuchtigkeit: " + this.weatherStation.getHumidity());
+        System.out.println("Druck: " + this.weatherStation.getPressure());
         System.out.println("_______________________________________");
     }
-    
+
+    public void removeDisplay() {
+        this.weatherStation.deleteDisplay(this);
+    }
+
 }
