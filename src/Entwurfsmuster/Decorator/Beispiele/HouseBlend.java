8165785a30eb5f2b79dd6aa60edeadfc151ff141
martinkribs <martin.kribs.mk@gmail.com>
2023-06-02 10:20:34 +0200

    coffee example

---
+++ src/Entwurfsmuster/Decorator/Beispiele/HouseBlend.java
@@ -0,0 +1,9 @@
+package Entwurfsmuster.Decorator.Beispiele;
+
+public class HouseBlend extends Beverage {
+
+	public HouseBlend() {
+		super("Hausgemacht nach alter Tradition", 1.99);
+	}
+
+}
---
+++ src/Entwurfsmuster/Decorator/Beispiele/Zutat.java
@@ -0,0 +1,9 @@
+package Entwurfsmuster.Decorator.Beispiele;
+
+public interface Zutat {
+    double getPreis();
+    String getBeschreibung();
+}
+
+// Feuer frei :P
+// pewpew ;-)
