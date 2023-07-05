2f0a6a7d9f268e5e9fe85ff43f029602ef7dcf4e
martinkribs <martin.kribs.mk@gmail.com>
2023-07-05 15:15:42 +0200

    changed wildcards and coments

--- src/Generics/Uebungen/PrintAndSum.java
+++ src/Generics/Uebungen/PrintAndSum.java
@@ -7,37 +7,53 @@ public class PrintAndSum {
 
     /**
      * main method
+     * 
      * @param args String array
      */
     public static void main(String[] args) {
-        //set
+        // set
         Paar<String, String> geWa = new Paar<>("Getrud", "Walter");
-        //print
+        // print
         print(geWa);
-        //set
+        // set
         Paar<Integer, Integer> test = new Paar<>(1, 2);
-        //sum
+        // sum
+        print(test);
         System.out.println(sum(test));
     }
 
     /**
      * print values of Paar
+     * 
      * @param p Paar
      */
-    public static void print(Paar<?, ?> p) {
-        Object v1 = p.getValue1();
-        Object v2 = p.getValue2();
+    public static <E> void print(Paar<E, E> p) {
+        E v1 = p.getValue1();
+        E v2 = p.getValue2();
         System.out.println("(" + v1 + ", " + v2 + ")");
     }
 
     /**
      * sum method
+     * 
      * @param p Paar
      * @return sum of values in Paar
      */
     public static double sum(Paar<? extends Number, ? extends Number> p) {
         double v1 = p.getValue1().doubleValue();
         double v2 = p.getValue2().doubleValue();
+        return v1 + v2;
+    }
+
+     /**
+     * sum method
+     * 
+     * @param p Paar
+     * @return sum of values in Paar
+     */
+    public static double sumExplicit(Paar<Double , Double> p) {
+        double v1 = p.getValue1();
+        double v2 = p.getValue2();
         return v1 + v2;
     }
 }
